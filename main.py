0cb9ad8bdbf3476ecaf28ef0173c4fca726fb899
Mauricio Cruz <mawcp7@gmail.com>
2024-01-14 20:29:36 -0600

    Add genetic algorithm execution and plot function

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
 import tkinter as tk
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import math as math
+from algorithm import genetic_algorithm
+
+
+def execute():
+    plot_function(custom_function)
+
+
+def plot_function(custom_function):
+    genetic_algorithm()
+
+    try:
+        x_values = np.linspace(-10, 10)
+        y_values = [custom_function(x) for x in x_values]
+
+        # Limpia la figura anterior
+        for widget in frame_plot.winfo_children():
+            widget.destroy()
+
+        # Crea una nueva figura y la muestra en el área designada
+        fig, ax = plt.subplots(figsize=(6, 4))
+        ax.plot(x_values, y_values, label="Custom Function")
+        plt.grid(True)
+        ax.set_title("Gráfica de la función")
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        ax.legend()
+
+        canvas = FigureCanvasTkAgg(fig, master=frame_plot)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+    except Exception as e:
+        print("Error al graficar la función:", e)
+
+
+def custom_function(x):
+    return x**3 * np.sin(x) + x**2 * np.cos(x)
+
 
 window = tk.Tk()
 window.title("Algoritmo Genético")
@@ -12,7 +50,7 @@ left_frame = tk.Frame(window)
 left_frame.pack(side=tk.LEFT, padx=10)
 
 frame1 = tk.LabelFrame(left_frame, text="Parámetros")
-frame1.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
+frame1.grid(row=0, column=0, padx=20, pady=30, sticky="ew")
 
 # label_func = tk.Label(left_frame, text="Funcion:")
 # label_func.pack(anchor="w")
@@ -22,46 +60,48 @@ frame1.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
 resolution_label = tk.Label(frame1, text="Resolucion inicial:")
 resolution_label.grid(row=1, column=0, sticky="w")
 resolution_entry = tk.Entry(frame1)
-resolution_entry.grid(row=1, column=1, sticky="w")
+resolution_entry.grid(row=1, column=1, padx=10, pady=10, sticky="w")
 
 generations_label = tk.Label(frame1, text="Numero de generaciones:")
 generations_label.grid(row=2, column=0, sticky="w")
 generations_entry = tk.Entry(frame1)
-generations_entry.grid(row=2, column=1, sticky="w")
+generations_entry.grid(row=2, column=1, padx=10, pady=10, sticky="w")
 
 interval_label = tk.Label(frame1, text="Intervalo:")
 interval_label.grid(row=3, column=0, sticky="w")
 
 interval_a_entry = tk.Entry(frame1)
-interval_a_entry.grid(row=3, column=1, sticky="w")
+interval_a_entry.grid(row=3, column=1, padx=10, sticky="w")
 
 interval_b_entry = tk.Entry(frame1)
-interval_b_entry.grid(row=4, column=1, sticky="w")
+interval_b_entry.grid(row=4, column=1, padx=10, sticky="w")
 
 initial_population_label = tk.Label(frame1, text="Poblacion inicial:")
 initial_population_label.grid(row=5, column=0, sticky="w")
 initial_population_entry = tk.Entry(frame1)
-initial_population_entry.grid(row=5, column=1, sticky="w")
+initial_population_entry.grid(row=5, column=1, padx=10, pady=10, sticky="w")
 
 max_population_label = tk.Label(frame1, text="Poblacion maxima:")
 max_population_label.grid(row=6, column=0, sticky="w")
 max_population_entry = tk.Entry(frame1)
-max_population_entry.grid(row=6, column=1, sticky="w")
+max_population_entry.grid(row=6, column=1, padx=10, pady=10, sticky="w")
 
 crossover_prob_label = tk.Label(frame1, text="Probabilidad de cruza:")
 crossover_prob_label.grid(row=7, column=0, sticky="w")
 crossover_prob_entry = tk.Entry(frame1)
-crossover_prob_entry.grid(row=7, column=1, sticky="w")
+crossover_prob_entry.grid(row=7, column=1, padx=10, pady=10, sticky="w")
 
 mutation_prob_label = tk.Label(frame1, text="Probabilidad de mutación:")
 mutation_prob_label.grid(row=8, column=0, sticky="w")
 mutation_prob_entry = tk.Entry(frame1)
-mutation_prob_entry.grid(row=8, column=1, sticky="w")
+mutation_prob_entry.grid(row=8, column=1, padx=10, pady=10, sticky="w")
 
-mutation_per_gene_prob_label = tk.Label(frame1, text="Probabilidad de utación por Gen:")
+mutation_per_gene_prob_label = tk.Label(
+    frame1, text="Probabilidad de mutación por Gen:"
+)
 mutation_per_gene_prob_label.grid(row=9, column=0, sticky="w")
 mutation_per_gene_prob_entry = tk.Entry(frame1)
-mutation_per_gene_prob_entry.grid(row=9, column=1, sticky="w")
+mutation_per_gene_prob_entry.grid(row=9, column=1, padx=10, pady=10, sticky="w")
 
 
 method_label = tk.Label(frame1, text="Metodo:")
@@ -69,10 +109,10 @@ method_label.grid(row=10, column=0, sticky="w")
 method = tk.StringVar()
 method.set("Minimizar")
 method_menu = tk.OptionMenu(frame1, method, "Minimizar", "Maximizar")
-method_menu.grid(row=10, column=1, sticky="w")
+method_menu.grid(row=10, column=1, padx=10, sticky="w")
 
-execute_button = tk.Button(frame1, text="Ejecutar")
-execute_button.grid(row=11, column=0, padx=10, pady=10,  sticky="w")
+execute_button = tk.Button(frame1, text="Ejecutar", command=execute)
+execute_button.grid(row=11, column=0, padx=10, pady=10, sticky="w")
 
 # Sección de la derecha para la gráfica
 right_frame = tk.Frame(window)
