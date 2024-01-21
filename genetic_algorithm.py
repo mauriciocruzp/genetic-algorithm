4354a2c7b6323c2d7e48473e21dba7db2be9bf20
Mauricio Cruz <mawcp7@gmail.com>
2024-01-21 13:45:25 -0600

    Refactor genetic algorithm functions

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -3,29 +3,29 @@ import sympy as sp
 import random
 
 
-def calculate_bits_number(points_number):
+def calculate_bits_number(points_number): # Calcula el número de bits necesarios para representar los puntos
     return int(points_number).bit_length()
 
 
-def calculate_int(individual):
+def calculate_int(individual): # Calcula el número entero a partir de la representación binaria
     int_number = 0
     for i in range(len(individual)):
         int_number += int(individual[i]) * (2 ** (len(individual) - i - 1))
     return int_number
 
 
-def evaluate(func, value):
+def evaluate(func, value): # Evalúa la función en un valor dado
     x = sp.symbols('x')
     expression = sp.sympify(func)
     result = expression.subs(x, value)
     return result
 
 
-def calculate_x(a, index, new_resolution):
+def calculate_x(a, index, new_resolution): # Calcula el valor de x a partir del número entero
     return a + (index * new_resolution)
 
 
-def initialisation(initial_population, bits_number, a, new_resolution, func):
+def initialisation(initial_population, bits_number, a, new_resolution, func): # Inicializa la población
     population = []
     for _ in range(initial_population):
         individual_number = ""
@@ -42,11 +42,11 @@ def initialisation(initial_population, bits_number, a, new_resolution, func):
     return population
 
 
-def get_eligible(population, probability):
+def get_eligible(population, probability): # Obtiene los individuos que serán cruzados
     return [individual for individual in population if random.random() <= probability]
 
 
-def generate_pairs(population, crossover_probability):
+def generate_pairs(population, crossover_probability): # Genera las parejas de individuos que serán cruzados, usando la estrategia de A2
     eligible_individuals = get_eligible(population, crossover_probability)
     pairs = []
 
@@ -57,8 +57,8 @@ def generate_pairs(population, crossover_probability):
     return pairs
 
 
-def crossover(pairs):
-    crossover_point = 3
+def crossover(pairs): # Cruza los individuos, usando la estrategia C3, a partir de un punto de cruza fijo
+    crossover_point = 2
     new_individuals = []
 
     for pair in pairs:
@@ -68,7 +68,7 @@ def crossover(pairs):
     return new_individuals
 
 
-def exchange_bits(individual):
+def exchange_bits(individual): # Intercambia dos bits de un individuo
     individual_list = list(individual)
     index1 = random.randint(0, len(individual_list) - 1)
     index2 = random.randint(0, len(individual_list) - 1)
@@ -80,14 +80,14 @@ def exchange_bits(individual):
     return mutated_individual
 
 
-def mutation(individuals, individual_mutation_probability):
+def mutation(individuals, individual_mutation_probability): # Muta los individuos, usando la estrategia M2, intercambio de bits
     for i in range(len(individuals)):
         if random.random() <= individual_mutation_probability:
             individuals[i] = exchange_bits(individuals[i])
     return individuals
 
 
-def get_statistics(population, minimize):
+def get_statistics(population, minimize): # Obtiene las estadísticas de la población
     key = "f(x)"
     best_individual = min(population, key=lambda x: x[key]) if minimize else max(population, key=lambda x: x[key])
     worst_individual = max(population, key=lambda x: x[key]) if minimize else min(population, key=lambda x: x[key])
@@ -95,7 +95,7 @@ def get_statistics(population, minimize):
     return {"best": best_individual, "worst": worst_individual, "average": total_fitness / len(population)}
 
 
-def prunning(population, max_population, minimize):
+def prunning(population, max_population, minimize): # Elimina los individuos, usando la estrategia P2, eliminando de forma aleatoria pero manteniendo al mejor individuo
     temporal_population = []
     for individual in population:
         if individual not in temporal_population:
@@ -116,32 +116,32 @@ def prunning(population, max_population, minimize):
 def optimize(expression, initial_resolution, generations_number, min_range, max_range, initial_population, max_population,
                       crossover_probability, mutation_probability, minimize):
 
-    range_number = max_range - min_range
-    points_number = (range_number / initial_resolution) + 1
-    bits_number = calculate_bits_number(points_number)
-    resolution = range_number / ((2 ** bits_number) - 1)
+    range_number = max_range - min_range # Calcula el rango
+    points_number = (range_number / initial_resolution) + 1 # Calcula el número de puntos
+    bits_number = calculate_bits_number(points_number) # Calcula el número de bits necesarios para representar los puntos
+    resolution = range_number / ((2 ** bits_number) - 1) # Calcula la resolución
 
-    population = initialisation(initial_population, bits_number, min_range, resolution, expression)
+    population = initialisation(initial_population, bits_number, min_range, resolution, expression) # Inicializa la población
 
     statistics = []
-    statistics.append(get_statistics(population, minimize))
+    statistics.append(get_statistics(population, minimize)) # Obtiene las estadísticas de la población inicial
 
     for _ in range(generations_number):
-        pairs = generate_pairs(population, crossover_probability)
-        new_individuals = crossover(pairs)
-        mutated_individuals = mutation(new_individuals, mutation_probability)
+        pairs = generate_pairs(population, crossover_probability) # Genera las parejas de individuos que serán cruzados
+        new_individuals = crossover(pairs) # Cruza los individuos
+        mutated_individuals = mutation(new_individuals, mutation_probability) # Muta los individuos
 
         new_individuals = []
-        for individual in mutated_individuals:
+        for individual in mutated_individuals: # Calcula el número entero, el valor de x y el valor de f(x) de cada individuo
             index = calculate_int(individual)
             x = calculate_x(min_range, index, resolution)
             fx = evaluate(expression, x)
 
-            new_individuals.append({"number": individual, "index": index, "x": x, "f(x)": fx})
+            new_individuals.append({"number": individual, "index": index, "x": x, "f(x)": fx}) # Agrega el individuo a la población
 
-        population.extend(new_individuals)
-        statistics.append(get_statistics(population, minimize))
-        population = prunning(population, max_population, minimize)
+        population.extend(new_individuals) # Agrega los nuevos individuos a la población
+        statistics.append(get_statistics(population, minimize)) # Obtiene las estadísticas de la población
+        population = prunning(population, max_population, minimize) # Elimina los individuos
     print(population)
 
     return statistics, population
--- main.py
+++ main.py
@@ -4,48 +4,129 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from genetic_algorithm import optimize
 
+window = tk.Tk() #ventana principal
+window.title("Algoritmo Genético")
+window.geometry("1200x700")
+window.resizable(False, False)
 
-def plot_graphs(expression, initial_resolution, generations, a, b, initial_population, max_population, crossover_probability,
-                  mutation_probability, minimize):
-    statistics, population = optimize(expression, initial_resolution, generations, a, b, initial_population, max_population,
-                                               crossover_probability,
-                                               mutation_probability, minimize)
-    statistics = np.array(statistics)
+top_frame = tk.Frame(window) #frame superior, contiene el frame de los parámetros
+top_frame.pack(side=tk.TOP, padx=10)
+
+parameters_frame = tk.LabelFrame(top_frame, text="Parámetros") #frame de los parámetros
+parameters_frame.grid(row=0, column=0, padx=20, pady=30, sticky="ew")
+
+expression_label = tk.Label(parameters_frame, text="Funcion:")
+expression_label.grid(row=0, column=0, sticky="w")
+expression_entry = tk.Entry(parameters_frame) # entrada de la función
+expression_entry.grid(row=0, column=1, padx=10, pady=10, sticky="w")
+
+resolution_label = tk.Label(parameters_frame, text="Resolucion inicial:")
+resolution_label.grid(row=1, column=0, sticky="w")
+resolution_entry = tk.Entry(parameters_frame) # entrada de la resolución
+resolution_entry.grid(row=1, column=1, padx=10, pady=10, sticky="w")
+
+generations_label = tk.Label(parameters_frame, text="Generaciones:")
+generations_label.grid(row=2, column=0, sticky="w")
+generations_entry = tk.Entry(parameters_frame)  # entrada de las generaciones
+generations_entry.grid(row=2, column=1, padx=10, pady=10, sticky="w")
+
+min_interval_label = tk.Label(parameters_frame, text="Min. intervalo:")
+min_interval_label.grid(row=0, column=2, sticky="w")
+min_interval_entry = tk.Entry(parameters_frame) # entrada del intervalo mínimo
+min_interval_entry.grid(row=0, column=3, padx=10, sticky="w")
+
+max_interval_label = tk.Label(parameters_frame, text="Max. intervalo:")
+max_interval_label.grid(row=1, column=2, sticky="w")
+max_interval_entry = tk.Entry(parameters_frame) # entrada del intervalo máximo
+max_interval_entry.grid(row=1, column=3, padx=10, sticky="w")
+
+initial_population_label = tk.Label(parameters_frame, text="Poblacion inicial:")
+initial_population_label.grid(row=2, column=2, sticky="w")
+initial_population_entry = tk.Entry(parameters_frame) # entrada de la población inicial
+initial_population_entry.grid(row=2, column=3, padx=10, pady=10, sticky="w")
+
+max_population_label = tk.Label(parameters_frame, text="Poblacion maxima:")
+max_population_label.grid(row=0, column=4, sticky="w")
+max_population_entry = tk.Entry(parameters_frame) # entrada de la población máxima
+max_population_entry.grid(row=0, column=5, padx=10, pady=10, sticky="w")
+
+crossover_prob_label = tk.Label(parameters_frame, text="Prob. de cruza:")
+crossover_prob_label.grid(row=1, column=4, sticky="w")
+crossover_prob_entry = tk.Entry(parameters_frame) # entrada de la probabilidad de cruza
+crossover_prob_entry.grid(row=1, column=5, padx=10, pady=10, sticky="w")
+
+mutation_prob_label = tk.Label(parameters_frame, text="Prob. de mutación:")
+mutation_prob_label.grid(row=2, column=4, sticky="w")
+mutation_prob_entry = tk.Entry(parameters_frame) # entrada de la probabilidad de mutación
+mutation_prob_entry.grid(row=2, column=5, padx=10, pady=10, sticky="w")
+
+maximize = tk.StringVar()
+maximize.set("Maximizar")
+max_menu = tk.OptionMenu(parameters_frame, maximize, "Maximizar", "Minimizar") # menú de maximizar o minimizar
+max_menu.grid(row=0, column=7, padx=10, sticky="w")
+
+
+def print_graphs(
+    expression,
+    initial_resolution,
+    generations_number,
+    max_range,
+    min_range,
+    initial_population,
+    max_population,
+    crossover_probability,
+    mutation_probability,
+    minimize,
+): # función que imprime las gráficas
+    statistics, population = optimize(
+        expression,
+        initial_resolution,
+        generations_number,
+        max_range,
+        min_range,
+        initial_population,
+        max_population,
+        crossover_probability,
+        mutation_probability,
+        minimize,
+    ) # se obtienen las estadísticas y la población
+    statistics = np.array(statistics) # se convierten a numpy arrays
 
     population = np.array(population)
 
-    for widget in frame_plot.winfo_children():
+    for widget in frame_plot.winfo_children(): # se eliminan los widgets de la gráfica
         widget.destroy()
 
+    # grafica de la aptitud del fitness
     fig, ax = plt.subplots(figsize=(6, 4))
     plt.grid(True)
     ax.set_title("Aptitud del fitness")
     ax.set_xlabel("Generación")
     ax.set_ylabel("Fitness")
 
-    generations = generations + 1
+    generations_number = generations_number + 1
 
-    generations = np.arange(0, generations, 1)
+    generations_number = np.arange(0, generations_number, 1)
     best = np.array([])
     worst = np.array([])
     average = np.array([])
 
-    for i in range(len(statistics)):
+    for i in range(len(statistics)): # se obtienen los mejores, peores y promedios de cada generación
         best = np.append(best, statistics[i]["best"]["f(x)"])
         worst = np.append(worst, statistics[i]["worst"]["f(x)"])
         average = np.append(average, statistics[i]["average"])
 
-    ax.plot(generations, best, label="Mejor")
-    ax.plot(generations, worst, label="Peor")
-    ax.plot(generations, average, label="Promedio")
+    ax.plot(generations_number, best, label="Mejor") # se grafican
+    ax.plot(generations_number, worst, label="Peor")
+    ax.plot(generations_number, average, label="Promedio")
 
-    ax.legend()
+    ax.legend() # se agrega la leyenda
 
     canvas = FigureCanvasTkAgg(fig, master=frame_plot)
     canvas.draw()
     canvas.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
 
-
+    # grafica de la población final
     fig, ax = plt.subplots(figsize=(6, 4))
     plt.grid(True)
     ax.set_title("Población final")
@@ -56,7 +137,7 @@ def plot_graphs(expression, initial_resolution, generations, a, b, initial_popul
     y = np.array([])
 
     for i in range(len(population)):
-        if a <= population[i]["x"] <= b:
+        if max_range <= population[i]["x"] <= min_range:
             x = np.append(x, population[i]["x"])
             y = np.append(y, population[i]["f(x)"])
 
@@ -67,91 +148,39 @@ def plot_graphs(expression, initial_resolution, generations, a, b, initial_popul
     canvas.get_tk_widget().pack(side=tk.RIGHT, fill=tk.BOTH, expand=1)
 
 
-window = tk.Tk()
-window.title("Algoritmo Genético")
-window.geometry("1200x700")
-window.resizable(False, False)
-
-parameter_frame = tk.Frame(window)
-parameter_frame.pack(side=tk.TOP, padx=10)
-
-frame1 = tk.LabelFrame(parameter_frame, text="Parámetros")
-frame1.grid(row=0, column=0, padx=20, pady=30, sticky="ew")
-
-func_label = tk.Label(frame1, text="Funcion:")
-func_label.grid(row=0, column=0, sticky="w")
-func_entry = tk.Entry(frame1)
-func_entry.grid(row=0, column=1, padx=10, pady=10, sticky="w")
-
-resolution_label = tk.Label(frame1, text="Resolucion inicial:")
-resolution_label.grid(row=1, column=0, sticky="w")
-resolution_entry = tk.Entry(frame1)
-resolution_entry.grid(row=1, column=1, padx=10, pady=10, sticky="w")
-
-generations_label = tk.Label(frame1, text="Generaciones:")
-generations_label.grid(row=2, column=0, sticky="w")
-generations_entry = tk.Entry(frame1)
-generations_entry.grid(row=2, column=1, padx=10, pady=10, sticky="w")
-
-interval_a_label = tk.Label(frame1, text="Min. intervalo:")
-interval_a_label.grid(row=0, column=2, sticky="w")
-interval_a_entry = tk.Entry(frame1)
-interval_a_entry.grid(row=0, column=3, padx=10, sticky="w")
-
-interval_b_label = tk.Label(frame1, text="Max. intervalo:")
-interval_b_label.grid(row=1, column=2, sticky="w")
-interval_b_entry = tk.Entry(frame1)
-interval_b_entry.grid(row=1, column=3, padx=10, sticky="w")
-
-initial_population_label = tk.Label(frame1, text="Poblacion inicial:")
-initial_population_label.grid(row=2, column=2, sticky="w")
-initial_population_entry = tk.Entry(frame1)
-initial_population_entry.grid(row=2, column=3, padx=10, pady=10, sticky="w")
-
-max_population_label = tk.Label(frame1, text="Poblacion maxima:")
-max_population_label.grid(row=0, column=4, sticky="w")
-max_population_entry = tk.Entry(frame1)
-max_population_entry.grid(row=0, column=5, padx=10, pady=10, sticky="w")
-
-crossover_prob_label = tk.Label(frame1, text="Prob. de cruza:")
-crossover_prob_label.grid(row=1, column=4, sticky="w")
-crossover_prob_entry = tk.Entry(frame1)
-crossover_prob_entry.grid(row=1, column=5, padx=10, pady=10, sticky="w")
-
-mutation_prob_label = tk.Label(frame1, text="Prob. de mutación:")
-mutation_prob_label.grid(row=2, column=4, sticky="w")
-mutation_prob_entry = tk.Entry(frame1)
-mutation_prob_entry.grid(row=2, column=5, padx=10, pady=10, sticky="w")
-
-method = tk.StringVar()
-method.set("Maximizar")
-method_menu = tk.OptionMenu(frame1, method, "Maximizar", "Minimizar")
-method_menu.grid(row=0, column=7, padx=10, sticky="w")
-
-
-def execute():
-    func = str(func_entry.get())
-    resolution = float(resolution_entry.get())
-    generations = int(generations_entry.get())
-    a = float(interval_a_entry.get())
-    b = float(interval_b_entry.get())
+def execute_algorithm():
+    expression = str(expression_entry.get()) # se obtienen los parámetros
+    initial_resolution = float(resolution_entry.get())
+    generations_number = int(generations_entry.get())
+    min_range = float(min_interval_entry.get())
+    max_range = float(max_interval_entry.get())
     initial_population = int(initial_population_entry.get())
     max_population = int(max_population_entry.get())
     crossover_probability = float(crossover_prob_entry.get())
     individual_mutation_probability = float(mutation_prob_entry.get())
-    minimize = method.get() == "Minimizar"
+    minimize = maximize.get() == "Minimizar"
 
-    plot_graphs(func, resolution, generations, a, b, initial_population, max_population, crossover_probability,
-                  individual_mutation_probability, minimize)
+    print_graphs(
+        expression,
+        initial_resolution,
+        generations_number,
+        min_range,
+        max_range,
+        initial_population,
+        max_population,
+        crossover_probability,
+        individual_mutation_probability,
+        minimize,
+    )
 
 
-execute_button = tk.Button(frame1, text="Ejecutar", command=execute)
+execute_button = tk.Button(parameters_frame, text="Ejecutar", command=execute_algorithm) # botón de ejecutar
 execute_button.grid(row=2, column=7, padx=10, pady=10, sticky="w")
 
-right_frame = tk.Frame(window)
-right_frame.pack(side=tk.BOTTOM, padx=10)
+bottom_frame = tk.Frame(window) # frame inferior, contiene el frame de las gráficas
+bottom_frame.pack(side=tk.BOTTOM, padx=10)
 
-frame_plot = tk.Frame(right_frame)
+frame_plot = tk.Frame(bottom_frame)
 frame_plot.pack()
 
-window.mainloop()
+window.mainloop() # se ejecuta la ventana
